e6a90e6ffa1600b5652d3f89d8b40431de0d38f8
Jared Andrews <jared.andrews07@gmail.com>
2026-01-11 18:35:21 -0600

    use cat rather than char

--- R/linePlot_module_ui.R
+++ R/linePlot_module_ui.R
@@ -6,7 +6,7 @@ linePlotInputsUI <- function(id, data, defaults = NULL, title = NULL, columns = 
 
     # Get numeric variables of data.
     num.choices <- c("", names(data)[unlist(lapply(data, is.numeric), use.names = FALSE)])
-    char.choices <- c("", names(data)[unlist(lapply(data, function(x) !is.numeric(x)), use.names = FALSE)])
+    cat.choices <- c("", names(data)[unlist(lapply(data, function(x) !is.numeric(x)), use.names = FALSE)])
     numeric.data <- data[, unlist(lapply(data, is.numeric), use.names = FALSE), drop = FALSE]
     max.y <- max(numeric.data, na.rm = TRUE)
     min.y <- min(numeric.data, na.rm = TRUE)
@@ -17,12 +17,12 @@ linePlotInputsUI <- function(id, data, defaults = NULL, title = NULL, columns = 
             selectInput(ns("y.value"), "Select Y values:", selected = names(data)[2], choices = names(data), multiple = TRUE),
             selectInput(ns("plot.type"), "Plot type: ", selected = "lines", choices = c("lines", "markers", "lines+markers")),
             selectInput(ns("line.type"), "Line type:", selected = "solid", choices = c("solid", "dot", "dash", "longdash", "dashdot", "longdashdot")),
-            selectInput(ns("group.by"), "Group by:", selected = char.choices[1], choices = char.choices), 
-            selectInput(ns("palette"), "Select palette:", selected = "Paired", choices = names(plotthis::palette_list)), 
+            selectInput(ns("group.by"), "Group by:", selected = cat.choices[1], choices = cat.choices),
+            selectInput(ns("palette"), "Select palette:", selected = "Paired", choices = names(plotthis::palette_list)),
             switchInput(ns("flip.y"), "Flip Y axis:", value = FALSE),
             switchInput(ns("flip.x"), "Flip X axis:", value = FALSE),
             switchInput(ns("order.by"), "Order plot by:", value = FALSE, offLabel = "x axis", onLabel = "y axis"),
-            selectInput(ns("facet.by"), "Facet by: ", selected = "", choices = char.choices),
+            selectInput(ns("facet.by"), "Facet by: ", selected = "", choices = cat.choices),
             selectInput(ns("y.adjustment"), "Adjust the y axis:", selected = "", choices = c("", "log2", "log", "log10", "neg_log10", "log1p", "abs", "sqrt")),
             selectInput(ns("x.adjustment"), "Adjust the x axis:", selected = "", choices = c("", "log2", "log", "log10", "neg_log10", "log1p", "abs", "sqrt"))
         ),
