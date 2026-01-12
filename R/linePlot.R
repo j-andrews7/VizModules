109bd048c953658e9973602025b4f5d7b70ad703
Jared Andrews <jared.andrews07@gmail.com>
2026-01-11 23:01:10 -0600

    dramatically simplify lineplot logic

--- R/linePlot.R
+++ R/linePlot.R
@@ -6,36 +6,73 @@
 #'
 #' @export
 #' @author Jacob Martin
-linePlot <- function(reactive.data, x.value, y.value, plot.mode, line.type, colour.group.by, palette.selection, show.legend, facet.by = NULL,
+linePlot <- function(reactive.data, x, y, plot.mode, line.type, colour.group.by, palette.selection, show.legend, facet.by = NULL,
                      axis.showline = TRUE, axis.mirror = TRUE, axis.linecolor = "black", axis.linewidth = 0.5, axis.tickfont.size = 12,
                      axis.tickfont.color = "black", axis.tickfont.family = "Arial", axis.tickangle.x = 0, axis.tickangle.y = 0, axis.ticks = "outside",
-                     axis.tickcolor = "black", axis.ticklen = 5, axis.tickwidth = 1, title.text = "Click To Edit Title", title.font.size = 14, title.font.family = "Arial",
-                     title.text.color = "black", axis.range.x = NULL, axis.range.y = NULL, y.title = NULL, x.title = NULL, flip.x = NULL, flip.y = NULL,
-                     x.adjustment = NULL, y.adjustment = NULL, x.input = NULL, y.input = NULL, order.by = NULL) {
+                     axis.tickcolor = "black", axis.ticklen = 5, axis.tickwidth = 1, title.text = "", title.font.size = 14, title.font.family = "Arial",
+                     title.text.color = "black", y.title = NULL, x.title = NULL, flip.x = FALSE, flip.y = FALSE,
+                     x.adjustment = NULL, y.adjustment = NULL, color.adjustment = NULL, order.by = NULL) {
     # Unique x axis styling for linePlot:
     xaxis_style <- list(
         showline = axis.showline, mirror = axis.mirror, linecolor = axis.linecolor, linewidth = axis.linewidth,
         tickfont = list(size = axis.tickfont.size, color = axis.tickfont.color, family = axis.tickfont.family),
         tickangle = axis.tickangle.x, ticks = axis.ticks, tickcolor = axis.tickcolor, ticklen = axis.ticklen, tickwidth = axis.tickwidth,
-        range = axis.range.x, title = x.title, autorange = flip.x
+        title = x.title, autorange = NULL
     )
 
     # Y axis styling by editing unique aspects of the x axis styling
     yaxis_style <- xaxis_style
     yaxis_style$tickangle <- axis.tickangle.y
-    yaxis_style$range <- axis.range.y
     yaxis_style$title <- y.title
 
+    if (flip.x) {
+        xaxis_style$autorange <- "reversed"
+    }
+
+    if (flip.y) {
+        yaxis_style$autorange <- "reversed"
+    }
+
     # Making axis adjustments if the parameters are not NULL
-    if (!is.null(x.adjustment)) {
-        reactive.data <- .adjust_column_values(df = reactive.data, col_names = x.input, transformation = x.adjustment)
+    if (!is.null(x.adjustment) && x.adjustment != "") {
+        reactive.data <- .adjust_column_values(df = reactive.data, x.col = x, x.adj.fun = x.adjustment)
+        x.new <- x
+        for (i in seq_along(x)) {
+            adj_name <- paste(x[i], "adj", sep = ".")
+            if (adj_name %in% names(reactive.data)) {
+                x.new[i] <- adj_name
+            }
+        }
+        x <- x.new
     }
-    if (!is.null(y.adjustment)) {
-        reactive.data <- .adjust_column_values(df = reactive.data, col_names = y.input, transformation = y.adjustment)
+
+    if (!is.null(y.adjustment) && y.adjustment != "") {
+        reactive.data <- .adjust_column_values(df = reactive.data, y.col = y, y.adj.fun = y.adjustment)
+        y.new <- y
+        for (i in seq_along(y)) {
+            adj_name <- paste(y[i], "adj", sep = ".")
+            if (adj_name %in% names(reactive.data)) {
+                y.new[i] <- adj_name
+            }
+        }
+        y <- y.new
     }
+
+    if (!is.null(color.adjustment) && color.adjustment != "") {
+        reactive.data <- .adjust_column_values(df = reactive.data, color.col = colour.group.by, color.adj.fun = color.adjustment)
+        colour.group.by.new <- colour.group.by
+        for (i in seq_along(colour.group.by)) {
+            adj_name <- paste(colour.group.by[i], "adj", sep = ".")
+            if (adj_name %in% names(reactive.data)) {
+                colour.group.by.new[i] <- adj_name
+            }
+        }
+        colour.group.by <- colour.group.by.new
+    }
+
     order.cols <- order.by
     if (is.null(order.cols)) {
-        order.cols <- x.input
+        order.cols <- x
     }
 
     plot_data <- reactive.data
@@ -43,35 +80,17 @@ linePlot <- function(reactive.data, x.value, y.value, plot.mode, line.type, colo
         plot_data <- reactive.data[order(reactive.data[[order.cols[1]]]), ]
     }
 
-    # Finding Axis min and max if all columns are numeric
-    # X values
-    if (all(sapply(plot_data[x.input], is.numeric))) {
-        min_vals_x <- sapply(plot_data[x.input], min, na.rm = TRUE)
-        max_vals_x <- sapply(plot_data[x.input], max, na.rm = TRUE)
-    } else {
-        min_vals_x <- NULL
-        max_vals_x <- NULL
-    }
-    # Y values:
-    if (all(sapply(plot_data[y.input], is.numeric))) {
-        min_vals_y <- sapply(plot_data[y.input], min, na.rm = TRUE)
-        max_vals_y <- sapply(plot_data[y.input], max, na.rm = TRUE)
-    } else {
-        min_vals_y <- NULL
-        max_vals_y <- NULL
-    }
+    multi_axis <- xor(length(x) > 1, length(y) > 1)
 
-    multi_axis <- xor(length(x.input) > 1, length(y.input) > 1)
-
-    if (!is.null(facet.by) && facet.by != "") {
+    if (!is.null(facet.by) && facet.by != "" && !multi_axis) {
         # Split data by facet variable
         facet_levels <- unique(plot_data[[facet.by]])
         plots <- lapply(facet_levels, function(level) {
             facet_data <- plot_data[plot_data[[facet.by]] == level, ]
             plot_ly(
                 data = facet_data,
-                x = x.value,
-                y = y.value,
+                x = reformulate(x),
+                y = reformulate(y),
                 type = "scatter",
                 mode = plot.mode,
                 line = list(dash = line.type),
@@ -85,8 +104,8 @@ linePlot <- function(reactive.data, x.value, y.value, plot.mode, line.type, colo
     } else {
         fig <- plot_ly(
             data = plot_data,
-            x = x.value,
-            y = y.value,
+            x = reformulate(x),
+            y = reformulate(y),
             type = "scatter",
             mode = plot.mode,
             line = list(dash = line.type),
@@ -97,18 +116,18 @@ linePlot <- function(reactive.data, x.value, y.value, plot.mode, line.type, colo
     }
 
     if (multi_axis) {
-        if (length(x.input) > 1) {
+        if (length(x) > 1) {
             fig <- fig |> add_trace(
                 data = plot_data,
-                x = plot_data[[x.input[1]]],
-                y = plot_data[[y.input[1]]],
+                x = plot_data[[x[1]]],
+                y = plot_data[[y[1]]],
                 type = "scatter",
                 mode = plot.mode,
                 line = list(dash = line.type),
-                name = x.input[1],
+                name = x[1],
                 showlegend = TRUE
             )
-            for (i in 2:length(x.input)) {
+            for (i in 2:length(x)) {
                 trace_data <- reactive.data
                 sort_column <- order.cols[1]
                 if (!is.null(order.cols) && length(order.cols) >= i && order.cols[i] %in% names(trace_data)) {
@@ -118,28 +137,28 @@ linePlot <- function(reactive.data, x.value, y.value, plot.mode, line.type, colo
                     trace_data <- trace_data[order(trace_data[[sort_column]]), ]
                 }
                 fig <- fig |> add_trace(
-                    x = trace_data[[x.input[i]]],
-                    y = trace_data[[y.input[1]]],
+                    x = trace_data[[x[i]]],
+                    y = trace_data[[y[1]]],
                     type = "scatter",
                     mode = plot.mode,
                     line = list(dash = line.type, color = palette.selection[i]),
-                    name = x.input[i],
+                    name = x[i],
                     showlegend = TRUE
                 )
             }
         }
-        if (length(y.input) > 1) {
+        if (length(y) > 1) {
             fig <- fig |> add_trace(
                 data = plot_data,
-                x = plot_data[[x.input[1]]],
-                y = plot_data[[y.input[1]]],
+                x = plot_data[[x[1]]],
+                y = plot_data[[y[1]]],
                 type = "scatter",
                 mode = plot.mode,
                 line = list(dash = line.type),
-                name = y.input[1],
+                name = y[1],
                 showlegend = TRUE
             )
-            for (i in 2:length(y.input)) {
+            for (i in 2:length(y)) {
                 trace_data <- reactive.data
                 sort_column <- order.cols[1]
                 if (!is.null(order.cols) && length(order.cols) >= i && order.cols[i] %in% names(trace_data)) {
@@ -149,12 +168,12 @@ linePlot <- function(reactive.data, x.value, y.value, plot.mode, line.type, colo
                     trace_data <- trace_data[order(trace_data[[sort_column]]), ]
                 }
                 fig <- fig |> add_trace(
-                    x = trace_data[[x.input[1]]],
-                    y = trace_data[[y.input[i]]],
+                    x = trace_data[[x[1]]],
+                    y = trace_data[[y[i]]],
                     type = "scatter",
                     mode = plot.mode,
                     line = list(dash = line.type, color = palette.selection[i]),
-                    name = y.input[i],
+                    name = y[i],
                     showlegend = TRUE
                 )
             }
@@ -169,36 +188,9 @@ linePlot <- function(reactive.data, x.value, y.value, plot.mode, line.type, colo
         ),
         margin = list(t = 80),
         showlegend = TRUE,
-        xaxis = list( # ← KEY: Always works here
-            showline = axis.showline,
-            mirror = axis.mirror,
-            linecolor = axis.linecolor,
-            linewidth = axis.linewidth,
-            tickfont = list(size = axis.tickfont.size, color = axis.tickfont.color, family = axis.tickfont.family),
-            tickangle = axis.tickangle.x,
-            ticks = axis.ticks,
-            tickcolor = axis.tickcolor,
-            ticklen = axis.ticklen,
-            tickwidth = axis.tickwidth,
-            range = c(min_vals_x, max_vals_x),
-            title = x.title,
-            autorange = flip.x
-        ),
-        yaxis = list(
-            showline = axis.showline,
-            mirror = axis.mirror,
-            linecolor = axis.linecolor,
-            linewidth = axis.linewidth,
-            tickfont = list(size = axis.tickfont.size, color = axis.tickfont.color, family = axis.tickfont.family),
-            tickangle = axis.tickangle.y,
-            ticks = axis.ticks,
-            tickcolor = axis.tickcolor,
-            ticklen = axis.ticklen,
-            tickwidth = axis.tickwidth,
-            range = c(min_vals_y, max_vals_y),
-            title = y.title,
-            autorange = flip.y
-        )
+        xaxis = xaxis_style,
+        yaxis = yaxis_style
     )
+
     return(fig)
 }
