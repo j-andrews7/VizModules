ea785d3611b98c868cdcb9b47124521613945069
Jared Andrews <jared.andrews07@gmail.com>
2026-01-21 11:01:59 -0600

    missing docstring

--- R/SplitBarPlot_module_server.R
+++ R/SplitBarPlot_module_server.R
@@ -1,3 +1,26 @@
+#' Server logic for SplitBarPlot module
+#'
+#' @param id The ID for the Shiny module.
+#' @param data A `reactive` containing the data frame to plot.
+#' @param hide.inputs A character vector of input IDs to hide.
+#'   These will still be initialized and their values passed to the plot function,
+#'   but the user will not be able to see/adjust them in the UI.
+#' @param hide.tabs A character vector of tab names to hide.
+#'   Inputs in these tabs will still be initialized and their values passed to the plot function,
+#'   but the user will not be able to see/adjust them in the UI.
+#'
+#' @return The `moduleServer` function for the SplitBarPlot module.
+#'
+#' @import shiny
+#' @importFrom shinyjs hide
+#' @importFrom plotly ggplotly
+#'
+#' @seealso [plotthis::SplitBarPlot()], [vizModules::organize_inputs()],
+#' [vizModules::SplitBarPlotInputsUI()], [vizModules::SplitBarPlotOutputUI()],
+#' [vizModules::SplitBarPlotApp()]
+#'
+#' @export
+#' @author Jacob Martin
 SplitBarPlotServer <- function(id, data, hide.inputs = NULL, hide.tabs = NULL) {
     stopifnot(is.reactive(data))
 
