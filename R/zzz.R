40626e71bec7d74be3f508d09424c9fe481a742f
Jacob <jacob@martin-email.com>
2026-03-04 11:41:10 -0600

    shinyBS required

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-    # Register shinyBS resource path so that tipify() tooltips work.
-    # shinyBS registers its "sbs" resource path in .onAttach(), but since
-    # VizModules only imports (not attaches) shinyBS, .onAttach() never fires.
-    shiny::addResourcePath("sbs", system.file("www", package = "shinyBS"))
-}
