753ac0912a0172f8a59a29c8f6e9ced3e40b09a5
Jacob <jacob@martin-email.com>
2026-05-29 11:41:05 -0500

    commit browser()

--- R/linePlot_module_server.R
+++ R/linePlot_module_server.R
@@ -314,7 +314,7 @@ linePlotServer <- function(id, data, hide.inputs = NULL, hide.tabs = NULL, defau
                 error.width = isolate_fn(input$error.bar.width),
                 error.bar = isolate_fn(input$error.bar)
             )
-
+            browser()
             # Apply axis title font to shared facet annotation titles
             if (!is.null(facet.by) && nzchar(facet.by)) {
                 fig <- .apply_axis_title_to_annotations(fig, input, isolate_fn)
