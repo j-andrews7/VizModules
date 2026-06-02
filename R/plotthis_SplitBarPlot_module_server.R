c78ab55db86831b1cb44f5abb8819cf2f5ba59be
Jacob <jacob@martin-email.com>
2026-06-02 11:23:09 -0500

    .

--- R/plotthis_SplitBarPlot_module_server.R
+++ R/plotthis_SplitBarPlot_module_server.R
@@ -491,7 +491,6 @@ plotthis_SplitBarPlotServer <- function(id, data, hide.inputs = NULL, hide.tabs 
             config_list <- .add_plot_config(download.format = isolate_fn(input$download.format), include.modebar.buttons = TRUE, facet.by = facet.by)
             fig <- do.call(config, c(list(p = fig), config_list))
             fig <- .apply_plotly_newshape(fig, input, isolate_fn)
-
             return(fig)
         })
 
